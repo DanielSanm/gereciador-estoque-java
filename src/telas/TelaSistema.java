8b68bfba228e7d7e12895e7d81d9d953c13a6458
DanielSanm <danielelias3221@gmail.com>
2022-02-17 14:26:36 -0300

    testando sistema

--- src/telas/TelaSistema.java
+++ src/telas/TelaSistema.java
@@ -5,9 +5,11 @@ import usuarios.Usuario;
 public class TelaSistema {
 
 	public static void mostrarSistema() {
-		for(Usuario usuarios: Usuario.listaUsuarios) {
-			System.out.println(usuarios);
-		}
+//		for(Usuario usuarios: Usuario.listaUsuarios) {
+//			System.out.println(usuarios);
+//		}
+		
+		Usuario.listaUsuarios.forEach(System.out::println);
 		TelaInicial.iniciar();
 	}
 
